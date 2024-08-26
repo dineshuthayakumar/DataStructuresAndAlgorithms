7d105c2753d27ee57543af3c44b050910362595f
Dinesh Uthayakumar <udinesh5@gmail.com>
2024-08-26 20:55:20 +0530

    Added menu header. Added do while to help repeate the menu items

--- Helper/MenuHelper.cs
+++ Helper/MenuHelper.cs
@@ -9,6 +9,7 @@ namespace DataStructuresAndAlgorithms.MenuHelper
         public static void PrintMainMenu()
         {
             do {
+                PrintMenuHeader("Main Menu");
                 System.Console.WriteLine("1. Print Numbers");
                 System.Console.WriteLine("2. Sum of Numbers");
                 System.Console.WriteLine("3. Search");
@@ -21,7 +22,7 @@ namespace DataStructuresAndAlgorithms.MenuHelper
                         PrintNumbersMenu();
                         break;
                     case 2:
-                        PrintSubOfNumbersMenu();
+                        PrintSumOfNumbersMenu();
                         break;
                     case 3:
                         PrintSearchMenu();
@@ -31,78 +32,117 @@ namespace DataStructuresAndAlgorithms.MenuHelper
                         break;
                     case 9:
                         return;
+                    default: 
+                        PrintInvalidChoice();
+                        break;
                 }
             } while (true);
         }
 
         public static void PrintNumbersMenu()
         {
-            System.Console.WriteLine("1. Compare Iteration vs Recursion for Print Numbers");
-            System.Console.WriteLine("2. Back");
-            int choice = UserInterfaceHelper.GetANumberFromUser("Provide your choice:");
-            switch (choice)
-            {
-                case 1:
-                    CompareIterationVsRecursion.CompareIterationVsRecursionForPrintNumbers(1000);
-                    break;
-                case 2:
-                    return; 
-            }
+            do {
+                PrintMenuHeader("Print Numbers Menu");
+                System.Console.WriteLine("1. Compare Iteration vs Recursion for Print Numbers");
+                System.Console.WriteLine("2. Back");
+                int choice = UserInterfaceHelper.GetANumberFromUser("Provide your choice:");
+                switch (choice)
+                {
+                    case 1:
+                        CompareIterationVsRecursion.CompareIterationVsRecursionForPrintNumbers(1000);
+                        break;
+                    case 2:
+                        return;
+                    default: 
+                        PrintInvalidChoice();
+                        break;
+                }
+            } while (true);
         }
         
-        public static void PrintSubOfNumbersMenu()
+        public static void PrintSumOfNumbersMenu()
         {
-            System.Console.WriteLine("1. Compare Iteration vs Recursion for Sum of Numbers");
-            System.Console.WriteLine("2. Compare Tree Recursion vs Tail Recursion for Sum of Numbers");
-            System.Console.WriteLine("3. Back");
-            int choice = UserInterfaceHelper.GetANumberFromUser("Provide your choice:");
-            switch (choice)
-            {
-                case 1:
-                    CompareIterationVsRecursion.CompareIterationVsRecursionForSumOfNumbers(1000);
-                    break;
-                case 2:
-                    CompareTreeVsTailRecursion.CompareTreeVsTailRecursionForSumOfNumbers(1000);
-                    break;
-                case 3:
-                    return;
-            }
+            do {
+                PrintMenuHeader("Sum of Numbers Menu");
+                System.Console.WriteLine("1. Compare Iteration vs Recursion for Sum of Numbers");
+                System.Console.WriteLine("2. Compare Tree Recursion vs Tail Recursion for Sum of Numbers");
+                System.Console.WriteLine("3. Back");
+                int choice = UserInterfaceHelper.GetANumberFromUser("Provide your choice:");
+                switch (choice)
+                {
+                    case 1:
+                        CompareIterationVsRecursion.CompareIterationVsRecursionForSumOfNumbers(1000);
+                        break;
+                    case 2:
+                        CompareTreeVsTailRecursion.CompareTreeVsTailRecursionForSumOfNumbers(1000);
+                        break;
+                    case 3:
+                        return;
+                    default: 
+                        PrintInvalidChoice();
+                        break;
+                }
+            } while (true);
         }
 
         public static void PrintSearchMenu()
         {
-            System.Console.WriteLine("1. Linear Search");
-            System.Console.WriteLine("2. Binary Search Iteration vs Recursion");
-            System.Console.WriteLine("3. Back");
-            int choice = UserInterfaceHelper.GetANumberFromUser("Provide your choice:");
-            switch (choice)
-            {
-                case 1:
-                    CompareLinearSearchBinarySearch.Compare();
-                    break;
+            do {
+                PrintMenuHeader("Search Menu");
+                System.Console.WriteLine("1. Linear Search");
+                System.Console.WriteLine("2. Binary Search Iteration vs Recursion");
+                System.Console.WriteLine("3. Back");
+                int choice = UserInterfaceHelper.GetANumberFromUser("Provide your choice:");
+                switch (choice)
+                {
+                    case 1:
+                        CompareLinearSearchBinarySearch.Compare();
+                        break;
 
-                case 2:
-                    CompareLinearSearchBinarySearch.Compare();
-                    break;
+                    case 2:
+                        CompareLinearSearchBinarySearch.Compare();
+                        break;
 
-                case 3:
-                    return;
-            }
+                    case 3:
+                        return;
+                    default: 
+                        PrintInvalidChoice();
+                        break;
+                }
+            } while (true);
         }
 
         public static void PrintSortingMenu()
-        { 
-            System.Console.WriteLine("1. Compare All");
-            System.Console.WriteLine("2. Back");
-            int choice = UserInterfaceHelper.GetANumberFromUser("Provide your choice:");
-            switch (choice)
-            {
-                case 1:
-                    CompareSorting.CompareAll();
-                    break;
-                case 2:
-                    return;
-            }
+        {
+            do {
+                PrintMenuHeader("Sorting Menu");
+                System.Console.WriteLine("1. Compare All");
+                System.Console.WriteLine("2. Back");
+                int choice = UserInterfaceHelper.GetANumberFromUser("Provide your choice:");
+                switch (choice)
+                {
+                    case 1:
+                        CompareSorting.CompareAll();
+                        break;
+                    case 2:
+                        return;
+                    default: 
+                        PrintInvalidChoice();
+                        break;
+                }
+            }while(true);
+        }
+
+        private static void PrintInvalidChoice()
+        {
+            System.Console.WriteLine("Invalid choice. Please try again.");
+        }
+
+        private static void PrintMenuHeader(string menuHeader)
+        {
+            System.Console.WriteLine("-----------------------------------");
+            System.Console.WriteLine($"\t{menuHeader}");
+            System.Console.WriteLine("-----------------------------------");
         }
     }
 }
